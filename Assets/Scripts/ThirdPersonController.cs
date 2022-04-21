fb1b8e8af97610a66da092b0cd2ae64235dee58b
ashechol <ashechol@qq.com>
2022-04-21 20:43:12 +0800

    update

--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))]
 public class ThirdPersonController : MonoBehaviour
 {
     CharacterController characterController;
+    public InputAction moveAction;
 
     void Awake()
     {
         characterController = GetComponent<CharacterController>();
     }
 
-    
+    void Move()
+    {
+
+    }
+
 }
