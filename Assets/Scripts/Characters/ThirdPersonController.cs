6b9628ad14d5cac2600963fddeef03c6cef8fc3a
Ashechol <ashechol@qq.com>
2022-04-22 17:02:01 +0800

    update

--- Assets/Scripts/Characters/ThirdPersonController.cs
+++ Assets/Scripts/Characters/ThirdPersonController.cs
@@ -8,10 +8,12 @@ public class ThirdPersonController : MonoBehaviour
 {
     CharacterController _controller;
     InputAction _moveAction;
+    public InputReader input;
     public Animator anim;
 
     [Header("Settings")]
     public float speed = 1;
+    public float currentSpeed;
     public Vector2 moveInput;
 
     void Awake()
@@ -23,9 +25,10 @@ public class ThirdPersonController : MonoBehaviour
 
     void OnEnable()
     {
-        _moveAction.started += GetMoveInput;
-        _moveAction.performed += GetMoveInput;
-        _moveAction.canceled += GetMoveInput;
+        // _moveAction.started += GetMoveInputStarted;
+        // _moveAction.performed += GetMoveInputPerformed;
+        // _moveAction.canceled += GetMoveInputCanceled;
+
     }
 
     void Start()
@@ -35,20 +38,39 @@ public class ThirdPersonController : MonoBehaviour
 
     void Update()
     {
+
         Move();
     }
 
-    void GetMoveInput(InputAction.CallbackContext context)
+    void GetMoveInputStarted(InputAction.CallbackContext context)
     {
-        Debug.Log("Move");
         moveInput = context.ReadValue<Vector2>();
+        Debug.Log("Move Started" + moveInput);
+    }
+
+    void GetMoveInputPerformed(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        Debug.Log("Move Performed" + moveInput);
+    }
+
+    void GetMoveInputCanceled(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        Debug.Log("Move Canceled" + moveInput);
     }
 
     void Move()
     {
-        Vector3 fb = transform.forward * moveInput.y;
-        Vector3 lr = transform.right * moveInput.x;
-        _controller.Move((fb + lr) * speed * Time.deltaTime);
+        Vector3 direction = transform.forward * moveInput.y + transform.right * moveInput.x;
+        Vector3 selfDirection = new Vector3(moveInput.x, 0, moveInput.y);
+        currentSpeed = new Vector3(_controller.velocity.x, 0, _controller.velocity.y).magnitude;
+
+        _controller.Move(direction * speed * Time.deltaTime);
+        // transform.Translate(direction * speed * Time.deltaTime, Space.World);
+        // transform.Translate(selfDirection * speed * Time.deltaTime);
+        // transform.position += direction * speed * Time.deltaTime;
+
     }
 
 }
