d1a9463d81b2e84090ffcd59fb2ef1122c06efc1
ashechol <ashechol@qq.com>
2022-04-24 00:37:58 +0800

    update

--- Assets/Scripts/Characters/ThirdPersonController.cs
+++ Assets/Scripts/Characters/ThirdPersonController.cs
@@ -8,10 +8,24 @@ public class ThirdPersonController : MonoBehaviour
     CharacterController _controller;
     GameInput _input;
     Animator _anim;
+    Vector3 _velocity;
+
+    float _cameraPitch;
+    float _cameraYaw;
 
     [Header("Basic Settings")]
     public float speed = 3;
+    public float sensitivity = 1;
     public float gravity = -15;
+
+    [Header("Ground Check")]
+    public bool grounded;
+    public float groundedRadius;
+    public float groundedOffset;
+    public LayerMask groundedLayer;
+
+    [Header("Cinemachine")]
+    public GameObject cameraTarget;
 
     void Awake()
     {
@@ -20,19 +34,59 @@ public class ThirdPersonController : MonoBehaviour
         _anim = GetComponent<Animator>();
     }
 
+    void Start()
+    {
+
+    }
+
     void Update()
     {
+        GroundedCheck();
         Move();
+    }
+
+    void LateUpdate()
+    {
+        CameraControll();
+    }
+
+    void CameraControll()
+    {
+        _cameraYaw = _input.lookInput.x;
+        _cameraPitch = _input.lookInput.y;
+
+        cameraTarget.transform.Rotate(_cameraPitch, _cameraYaw, 0);
     }
 
     void Move()
     {
-        Vector3 direction = transform.forward * _input.moveInput.y + transform.right * _input.moveInput.x;
-        Vector3 vertical = Vector3.up * gravity * Time.deltaTime;
+        _velocity = transform.forward * _input.moveInput.y * speed;
 
-        _controller.Move(direction * speed * Time.deltaTime + vertical);
+        if (!grounded)
+            _velocity += Vector3.up * gravity;
+
+        _controller.Move(_velocity * Time.deltaTime);
+        transform.Rotate(Vector3.up, _input.moveInput.x * sensitivity * Time.deltaTime);
 
         _anim.SetFloat("speed", _controller.velocity.magnitude);
+    }
+
+    void GroundedCheck()
+    {
+        Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y + groundedOffset, transform.position.z);
+        grounded = Physics.CheckSphere(spherePosition, groundedRadius, groundedLayer, QueryTriggerInteraction.Ignore);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Color transparentGreen = new Color(0.0f, 1.0f, 0.0f, 0.35f);
+        Color transparentRed = new Color(1.0f, 0.0f, 0.0f, 0.35f);
+
+        if (grounded) Gizmos.color = transparentGreen;
+        else Gizmos.color = transparentRed;
+
+        Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y + groundedOffset, transform.position.z);
+        Gizmos.DrawSphere(spherePosition, groundedRadius);
     }
 
     //TODO: 地面监测
--- Assets/Scripts/Input/GameInput.cs
+++ Assets/Scripts/Input/GameInput.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,25 +6,41 @@ using UnityEngine.InputSystem;
 public class GameInput : MonoBehaviour
 {
     PlayerInput _input;
+    InputAction lookAction;
 
     public Vector2 moveInput;
     public Vector2 lookInput;
 
+    [HideInInspector]
+    public bool normalAttack;
+    public bool chargeAttack;
+
     void Awake()
     {
         _input = GetComponent<PlayerInput>();
+        lookAction = _input.actions["Look"];
     }
 
     void OnEnable()
     {
         _input.onActionTriggered += GetMoveInput;
         _input.onActionTriggered += GetLookInput;
+        _input.onActionTriggered += GetNormalAttackInput;
+
+        _input.actions["ChargeAttack"].performed += GetChargeAttackInput;
+        _input.actions["ChargeAttack"].canceled += GetChargeAttackInput;
+    }
+
+    void Update()
+    {
+
     }
 
     void OnDisable()
     {
         _input.onActionTriggered -= GetMoveInput;
         _input.onActionTriggered -= GetLookInput;
+        _input.onActionTriggered -= GetNormalAttackInput;
     }
 
     void GetMoveInput(InputAction.CallbackContext context)
@@ -34,7 +51,19 @@ public class GameInput : MonoBehaviour
 
     void GetLookInput(InputAction.CallbackContext context)
     {
+
         if (context.action.name == "Look")
             lookInput = context.ReadValue<Vector2>();
+    }
+
+    void GetNormalAttackInput(InputAction.CallbackContext context)
+    {
+
+        normalAttack = context.action.name == "NormalAttack" ? true : false;
+    }
+
+    void GetChargeAttackInput(InputAction.CallbackContext context)
+    {
+        normalAttack = context.action.name == "ChargeAttack" ? true : false;
     }
 }
